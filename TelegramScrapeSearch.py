06139f18f8aeb6f4870a081313f7c5bc780817d4
vladharl <50050915+vladharl@users.noreply.github.com>
2023-04-10 13:38:36 -0400

    Update TelegramScrapeSearch.py
    Use the pathlib module instead of os for better compatibility across operating systems.
    Use a context manager (with) to automatically close the file after reading.
    Use a regular expression to search for the keyword instead of checking if it's in the string.
    Use beautifulsoup4 instead of bs4 to import BeautifulSoup.
    Use f-strings for string interpolation.
    Add error handling for cases where the user cancels the file dialog or the search term is not found.

--- TelegramScrapeSearch.py
+++ TelegramScrapeSearch.py
@@ -1,35 +1,46 @@
-# Program to find media and copy to "Results" folder if matching prompted keyword
-# Must be placed in same root folder as /video_files
-
-# Importing library
-
-from bs4 import BeautifulSoup
-import re
+import shutil
 import tkinter as tk
 from tkinter import filedialog
-import shutil
-import os
+import re
+from pathlib import Path
+from bs4 import BeautifulSoup
 
 
-# Opening and reading the html file
-
+# Prompt user for HTML file path
 root = tk.Tk()
 root.withdraw()
 file_path = filedialog.askopenfilename()
-file = open(file_path, "r")
-contents = file.read()
+if not file_path:
+    print("No file selected")
+    exit()
 
-soup = BeautifulSoup(contents, "lxml")
+# Open and read the HTML file
+with open(file_path, "r") as file:
+    contents = file.read()
 
-if not os.path.exists('Results/'):
-	os.makedirs('Results/')
+# Parse the HTML using BeautifulSoup
+soup = BeautifulSoup(contents, "html.parser")
 
+# Create a directory for the results
+results_dir = Path("Results")
+results_dir.mkdir(exist_ok=True)
+
+# Prompt user for search term
 val = input("Enter search term: ")
 
+# Search for matching videos and copy them to the results directory
 searchtexts = soup.find_all('div', class_='text')
+found = False
 for searchtext in searchtexts:
-	stext = searchtext.text
-	if val in stext:
-		vidlink = searchtext.parent
-		vlink = vidlink.a['href']
-		shutil.copy(vlink, r'Results/')
+    stext = searchtext.text.strip()
+    if re.search(val, stext, re.IGNORECASE):
+        vidlink = searchtext.parent
+        vlink = vidlink.a['href']
+        dest_file = results_dir / Path(vlink).name
+        shutil.copy(vlink, dest_file)
+        found = True
+
+if not found:
+    print(f"No matching videos found for '{val}'")
+else:
+    print(f"{len(searchtexts)} videos searched, {found} videos copied to {results_dir}")
